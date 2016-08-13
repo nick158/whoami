46c414763d287293cabae1f2a43dd2c50449feec
nick158 <nicholasdlchan60@gmail.com>
2016-08-13 02:13:09 +0000

    comment

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
+    //final response object
     var obj = {};
+    //access the request headers language
     obj.lang = req.headers['accept-language'].split(',')[0];
+    //ip address
     obj.ipaddress = req.headers['x-forwarded-for'];
+    //find index of bracket 1 and 2, get everything in between which is OS platform
     var platform = "";
     var userAg = req.headers['user-agent'];
     var ind = userAg.indexOf('(');
     var ind2 = userAg.indexOf(')');
     obj.platform = userAg.substring(ind+1, ind2);
+    //send by json
     res.json(obj);
     
     res.end();
