5b2633f845d3705db0757fa9c2bca2b9a6f78c4d
nick158 <nicholasdlchan60@gmail.com>
2016-08-13 02:11:56 +0000

    completion

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
     var obj = {};
     obj.lang = req.headers['accept-language'].split(',')[0];
     obj.ipaddress = req.headers['x-forwarded-for'];
-    console.log(obj);
+    var platform = "";
+    var userAg = req.headers['user-agent'];
+    var ind = userAg.indexOf('(');
+    var ind2 = userAg.indexOf(')');
+    obj.platform = userAg.substring(ind+1, ind2);
+    res.json(obj);
+    
+    res.end();
+    return;
 });
 app.listen(8080);
